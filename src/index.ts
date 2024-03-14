ca4f6f6fa12a03353b82224b47cc234e4ce62a96
Mohamed Aziz Bechaib <azizbechaib711@gmail.com>
2024-03-14 10:02:41 +0100

    save

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { config } from "dotenv";
+
+config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT ?? 3000;
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
